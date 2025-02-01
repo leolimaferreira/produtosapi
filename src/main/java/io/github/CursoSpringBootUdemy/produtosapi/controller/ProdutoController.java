82d6368e84ed944e052cffe5809053fb21be70b7
leolimaferreira <leo2342005@gmail.com>
2025-02-01 20:02:58 -0300

    Initial commit

---
+++ src/main/java/io/github/CursoSpringBootUdemy/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,50 @@
+package io.github.CursoSpringBootUdemy.produtosapi.controller;
+
+import io.github.CursoSpringBootUdemy.produtosapi.model.Produto;
+import io.github.CursoSpringBootUdemy.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto) {
+        System.out.println("Produto recebido: " + produto.toString());
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
+    }
+}
